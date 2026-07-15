3550a782b02ea4d420112ce6d5bdf018142079cf
TnzinDEV <tnzin.github@gmail.com>
2026-07-15 18:23:37 -0300

    explicaçao de (def)

---
+++ def.py
@@ -0,0 +1,14 @@
+#uma funçao em python e como uma receita de bolo ou uma maquina que voce deixa guardada para usar sempre que precisa.
+
+
+#em vem de escrever o mesmo codigo varias vezes, voce o empacota dentro de uma funçao, da um nome a ele e o chamasempre que quiser.
+
+
+#como funciona? (fluxo em 4 passos)
+#definiçao (def): e onde vocecria e da um nome a ela.
+
+#Entrada (Parametros): os "ingredientes" que a maquina precisa para trabalhar (Opicional)
+
+#Processamento: o codigo dentro dela que faz o trabalho pesado.
+
+#saida: (return): O resultado que a maquina te entrega de volta (Opcional)
