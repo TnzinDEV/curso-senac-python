ec4d021e829710c32943c45774059bd712119c4e
TnzinDEV <tnzin.github@gmail.com>
2026-08-03 19:14:31 -0300

    aual isolada pos ferias

---
+++ prog61.py
@@ -0,0 +1,9 @@
+class Carros:
+    def__init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+    
+carro1 = Carros("toyota", "corola")
+carro2 = Carros("vw", "gol")
+
+print(f"A marca do seu Carro e {carro1.marca} e o modelo e {carro1,modelo}")
