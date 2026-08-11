879037ba8b640f0da98a9051aca641d4ffd51b60
TnzinDEV <tnzin.github@gmail.com>
2026-08-10 21:33:52 -0300

    aulinha html

--- uc2/html.py
+++ uc2/html.py
@@ -1,3 +1,71 @@
 #Aula de html css e anotaçoes
 #Baixar live server para rodar as paginas em html em tempo real
 #criar um arquivo chamado index.html
+#html e o que da estrutura a pagina
+#emmet sao atalhos se voce der ! e enter ele te fornece o seguinte codigo
+
+
+#<!DOCTYPE html>
+#<html lang="en">
+#<head>
+#    <meta charset="UTF-8">
+#    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#    <title>Document</title>
+#</head>
+#<body>
+    
+#</body>
+#</html>
+
+
+#mudar para pt-br
+
+#tudo dentro do body aparece no site
+#se voceclickar em go live no canto inferior direito da tela do vscode
+#voce abre uma previa do seu html
+
+
+
+#Todas as tags ficam entre <nome da tag>
+
+#          tags
+
+#tag - h1 (da um titulo)
+#vai da h1 a h6 
+#quanto menor o numero e menor a letra
+#sao apenas para titulos e sub titulos
+
+#<p>- para paragrafo
+
+#<header> - header e a entrada do site
+#demonstraçao ou apresentaçao
+
+#o site funciona como blocos 
+#no codigo parecem continuos porem e importante organizar
+
+#O cabeçalio fica dentro de <header>
+
+# a tag <nav> serve para navegaçao
+
+#<ul> lista nao coordenada
+
+#<hr> - faz uma linha
+
+#<main> - e o conteudo principal
+
+#<section> - sessao (ficam dentro da main)
+
+#<br/> = break
+
+#<!-- documentaçao e comentario--> - para comentar
+
+
+
+
+#                links
+
+
+#<a href="qualquer link"> - ativa o seu link a de ativaçao
+#target="_blank abrir nova guia
+#identificaçao <section id="primeira">
+#para chamar <li><a href="#primeira"
